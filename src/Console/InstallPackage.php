f13e1acc429e4d4b43c4b8c027e70afbd2f78c6a
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-08-28 14:37:52 -0300

    Added lseditor.css

--- src/Console/InstallPackage.php
+++ src/Console/InstallPackage.php
@@ -25,6 +25,10 @@ class InstallPackage extends Command
         copy(__DIR__.'/../../resources/js/lseditor.js', resource_path('js/lseditor.js'));
         $this->info("resources/js/lseditor.js created...");
 
+        $this->info("Copying lseditor.css to resources/css folder...");
+        copy(__DIR__.'/../../resources/css/lseditor.css', resource_path('css/lseditor.css'));
+        $this->info("resources/css/lseditor.css created...");
+
         if($this->ask("Do you want to update your vite.config.js automatically? Back it up first! [y/n]", "n") === "y")
         {
             $this->info("Trying to include 'resources/js/lseditor.js' in vite.config.js");
@@ -32,19 +36,20 @@ class InstallPackage extends Command
             if(str_contains($fContent, "resources/js/lseditor.js")){
                 $this->warn("Inclusion of 'resources/js/lseditor.js' already exists inside vite.config.js!");
             }else{
-                $fContent = str_replace("input: [", "input: [\n'resources/js/lseditor.js',", $fContent);
+                $fContent = str_replace("input: [", "input: [\n'resources/js/lseditor.js',\n'resources/css/lseditor.css',\n", $fContent);
                 file_put_contents(base_path('vite.config.js'), $fContent);
-                $this->info("'resources/js/lseditor.js' included in vite.config.js");
+                $this->info("'resources/js/lseditor.js' and 'resources/css/lseditor.css' included in vite.config.js");
                 $this->info("Building application... (Running npm run build)");
                 if(shell_exec('npm run build') === null)
                 {
                     $this->warn("Couldn't build application...");
                 }else{
                     $this->info("Application built!");
+                    $this->info("Add 'resources/js/lseditor.js' and 'resources/css/lseditor.css' to vite['resources/css/app.css'[...]] in layout");
                 }
             }
         }else{
-            $this->warn("Include 'resources/js/lseditor.js' in vite.config.js and run 'npm run build'");
+            $this->warn("Include 'resources/js/lseditor.js' and 'resources/css/lseditor.css' in vite.config.js and run 'npm run build'");
         }
     }
 }
