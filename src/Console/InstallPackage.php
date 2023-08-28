778e5ee7000c05f6c3e91e4d1d8555a3ff15ab74
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-08-27 21:34:38 -0300

    Installer test

--- src/Console/InstallPackage.php
+++ src/Console/InstallPackage.php
@@ -13,12 +13,17 @@ class InstallPackage extends Command
 
     public function handle()
     {
-        $this->info("Installing dependencies...");
-        if(shell_exec('composer install') === null)
+        $this->info("Installing editorjs/editorjs...");
+        if(shell_exec('npm i @editorjs/editorjs --save') === null)
         {
             $this->warn("Couldn't install dependencies... Please manually install them!");
             return;
         }
-        $this->info("Dependencies installed...");
+        $this->info("editorjs/editorjs installed...");
+
+        $this->info("Copying lseditor.js to resources/js folder...");
+        copy('../../resources/js/lseditor.js', resource_path('js/lseditor.js'));
+        $this->info("resources/js/lseditor.js created...");
+        $this->warn("Include 'resources/js/app.js' in vite.config.js");
     }
 }
