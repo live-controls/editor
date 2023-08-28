5ba9d5228d5be33230e788fb79b088ae783ac067
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-08-28 09:10:01 -0300

    Added better design

--- config/config.php
+++ config/config.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
-    'default_tools' => null, //Set the default tools that should be included in every editor if nothing else is set 
+    'default_tools' => null, //Set the default tools that should be included in every editor if nothing else is set
+    'save_button_style' => 'btn btn-success', //The style for the save button
+    'clear_button_style' => 'btn btn-error', //The style for the clear button 
 ];
--- resources/views/livewire/editor.blade.php
+++ resources/views/livewire/editor.blade.php
@@ -1,7 +1,7 @@
 <div>
     <script type="text/javascript">
         document.addEventListener("DOMContentLoaded", function() {
-            const editor = new window.EditorJS({
+            const {{ $holderId }}editor = new window.EditorJS({
                 /**
                  * Id of Element that should contain Editor instance
                  */
@@ -13,4 +13,12 @@
             });
         });
     </script>
+
+    <!-- LiveControls.Editor -->
+    <div class="bg-white rounded rounded-lg">
+        <div id="{{ $holderId }}"></div>
+        <button class="{{ config('livecontrols_editor.clear_button_style', 'bg-red text-white rounded p-4') }}">Clear</button>
+        <button class="{{ config('livecontrols_editor.save_button_style', 'bg-green text-white rounded p-4') }}">Save</button>
+    </div>
+    <!-- /LiveControls.Editor -->
 </div>
